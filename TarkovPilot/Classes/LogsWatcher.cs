c338c1abd6f62313ebe1b1845171db6485dab57f
Alexander Sveshnikov <diamondliquid@gmail.com>
2024-08-13 13:38:23 +0300

    LocationToMap upd

--- TarkovPilot/Classes/LogsWatcher.cs
+++ TarkovPilot/Classes/LogsWatcher.cs
@@ -24,13 +24,18 @@ namespace TarkovPilot
             { "sandbox_high", MapName.Ground_Zero },
             { "factory_day", MapName.Factory },
             { "factory_night", MapName.Factory },
+            { "factory4_day", MapName.Factory },
+            { "factory4_night", MapName.Factory },
             { "bigmap", MapName.Customs },
             { "woods", MapName.Woods },
             { "shoreline", MapName.Shoreline },
+            { "interchange", MapName.Interchange },
             { "shopping_mall", MapName.Interchange },
+            { "rezervbase", MapName.Reserve },
             { "rezerv_base", MapName.Reserve },
             { "laboratory", MapName.The_Lab },
             { "lighthouse", MapName.Lighthouse },
+            { "tarkovstreets", MapName.Streets_of_Tarkov },
             { "city", MapName.Streets_of_Tarkov },
         };
 
