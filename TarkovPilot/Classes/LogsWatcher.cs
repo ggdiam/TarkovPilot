feeab3a4bdedef380d6e15fcba5e760fb537ba53
Alexander Sveshnikov <diamondliquid@gmail.com>
2024-08-13 10:48:36 +0300

    LocationToMap update

--- TarkovPilot/Classes/LogsWatcher.cs
+++ TarkovPilot/Classes/LogsWatcher.cs
@@ -22,17 +22,16 @@ namespace TarkovPilot
         static Dictionary<string, string> LocationToMap = new Dictionary<string, string>
         {
             { "sandbox_high", MapName.Ground_Zero },
-            { "factory", MapName.Factory },
-            { "factory4_day", MapName.Factory },
-            { "factory4_night", MapName.Factory },
+            { "factory_day", MapName.Factory },
+            { "factory_night", MapName.Factory },
             { "bigmap", MapName.Customs },
             { "woods", MapName.Woods },
             { "shoreline", MapName.Shoreline },
-            { "interchange", MapName.Interchange },
-            { "reservebase", MapName.Reserve },
+            { "shopping_mall", MapName.Interchange },
+            { "rezerv_base", MapName.Reserve },
             { "laboratory", MapName.The_Lab },
             { "lighthouse", MapName.Lighthouse },
-            { "tarkovstreets", MapName.Streets_of_Tarkov }
+            { "city", MapName.Streets_of_Tarkov },
         };
 
         public static void Start()
