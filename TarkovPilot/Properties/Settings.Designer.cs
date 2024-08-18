3f455840ae6d4de63e650989c50249f93b996e49
Malov Gleb <gleb.malov@arsis.ru>
2024-08-18 17:39:19 +0500

    Change property false to true default

--- TarkovPilot/Properties/Settings.Designer.cs
+++ TarkovPilot/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace TarkovPilot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RemoveImage {
             get {
                 return ((bool)(this["RemoveImage"]));
